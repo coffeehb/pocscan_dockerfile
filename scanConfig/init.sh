da127a8a932a07979f9958bba3898942702a73fe
zhangzhe <zhangzhe@bilibili.com>
2016-05-18 17:47:24 +0800

    bishe

--- scanConfig/init.sh
+++ scanConfig/init.sh
@@ -2,7 +2,4 @@
 cd /www/ && python /www/manage.py makemigrations
 cd /www/ &&  python /www/manage.py migrate --noinput
 
-chmod -R 777 /www 
-echo "root:password"|chpasswd
-
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('root', 'admin@example.com', 'password')" | python /www/manage.py shell
